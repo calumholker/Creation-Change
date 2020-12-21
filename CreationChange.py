fa2d71ed43acc26a42849eeb906547982d824a71
Calum <chmail@holk.co.uk>
2020-12-21 15:26:43 +0000

    Fixed Space Split Issue

--- CreationChange.py
+++ CreationChange.py
@@ -2,23 +2,6 @@ from subprocess import call
 import os
 from datetime import datetime
 
-def space_invader(dir):
-    restart = 0
-    for file in os.listdir(dir):
-        fullPath = os.path.join(dir, file)
-        if ' ' in fullPath:
-            os.rename(fullPath, fullPath.replace(' ', '_'))
-            restart = 1
-            break
-        elif ';' in fullPath:
-            os.rename(fullPath, fullPath.replace(';', '_'))
-            restart = 1
-            break
-        elif os.path.isdir(fullPath):
-            space_invader(fullPath)
-    if restart == 1:
-        space_invader(dir)
-
 def exorcist(fullPath):
     filename = (os.path.basename(fullPath))[0:24]
     format = 'clip-%Y-%m-%d_%H_%M_%S'
@@ -26,7 +9,6 @@ def exorcist(fullPath):
     return date_time
 
 directory = input("enter dir:")
-space_invader(directory)
 
 for subdir, dirs, files in os.walk(directory):
     for file in files:
@@ -34,7 +16,8 @@ for subdir, dirs, files in os.walk(directory):
         if filepath.endswith(".dv") or filepath.endswith(".mov"):
             try:
                 new_date = exorcist(filepath)
-                os.system('SetFile -d "{}" {}'.format(new_date.strftime('%m/%d/%Y %H:%M:%S'), filepath))
+                command = 'Touch -t ' + new_date.strftime('%Y%m%d%H%M.%S') + ' "' + filepath + '"'
+                call(command, shell=True)
             except:
                 if 'Cache' not in filepath:
                     print("UNORTHADOX FILE NAME DETECTED: " + filepath)
